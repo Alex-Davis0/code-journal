1c0fe780c1e0f9cb7479efad6f5459564fc73ca2
Alexander <amchops@comcast.net>
2021-12-21 13:00:21 -0800

    Fix all errors.#1

--- js/main.js
+++ js/main.js
@@ -3,8 +3,28 @@
 
 var $url = document.querySelector('.photo-url');
 var $img = document.querySelector('.img');
+
 $url.addEventListener('input', function (event) {
 
   $img.setAttribute('src', event.target.value);
 
+});
+
+var $form = document.querySelector('.form');
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var formvalue = {
+    entryId: data.nextEntryId,
+    title: $form.elements.Title.value,
+    photourl: $form.elements.photoUrl.value,
+    notes: $form.elements.Note.value
+
+  };
+
+  data.nextEntryId++;
+  data.entries.push(formvalue);
+  localStorage.setItem('data-profile', JSON.stringify(data));
+  $form.reset();
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
 });
