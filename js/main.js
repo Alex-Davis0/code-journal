98ca167b24565422e7837ce9fbce1fece591c127
Alexander <amchops@comcast.net>
2021-12-20 17:33:06 -0800

    Ensure that the form looks nice on both mobile-sized and desktop-sized screens. #5

--- js/main.js
+++ js/main.js
@@ -1,2 +1,10 @@
 /* global data */
 /* exported data */
+
+var $url = document.querySelector('.photo-url');
+var $img = document.querySelector('.img');
+$url.addEventListener('input', function (event) {
+
+  $img.setAttribute('src', event.target.value);
+
+});
